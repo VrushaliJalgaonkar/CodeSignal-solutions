89f57ce90daaea20ba364cfc046422ad99283e53
Vrushali Jalgaonkar <vrushali.jalgaonkar16@gmail.com>
2023-11-03 00:41:43 +0900

    Create adjacentElementsProduct.java

---
+++ adjacentElementsProduct.java
@@ -0,0 +1,8 @@
+int solution(int[] inputArray) {
+    int max = Integer.MIN_VALUE;
+    for(int i = 1;i<inputArray.length;i++){
+        int prod = inputArray[i-1] * inputArray[i];
+        max = Math.max(max,prod);
+    }
+    return max;
+}
